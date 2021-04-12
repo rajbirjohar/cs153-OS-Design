3b8367851afdfd48d04df14e787b5e4b613431eb
r-jo <rdrsjck@gmail.com>
2021-04-12 11:38:20 -0700

    Initial commit

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "user.h"
+
+int main (int argc, char **argv) {
+    printf(1,"Hello World!\nThis is Rajbir's first C program.\n"); // fd,std out
+    exit(); //exit the program
+}
+
+// 1. Boot xv6
+// $ cd xv6
+// $make qemu-nox
+
+// 2. To Quit
+// $ ctrl+a x
+
+// 3. To Debug
+// [sledge] make clean
+// [sledge] make qemu-nox-gdb (keep this running)
+// [local] ssh -L 28868:localhost:28868 sjoha005@sledge.ucr.edu
+// [local] password:
+// [local] -> [sledge] (keep this running)
+
